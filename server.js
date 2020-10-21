430c936417d0fbfb7fd25df3fb7834ac25587ab5
Matt G <matt.gear@live.co.uk>
2020-10-21 17:45:51 +0100

    Initial set up

---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+// Set port to env variable or 5000
+const port = process.env.PORT || 5000;
+
+// Middleware to print incoming requests to server console
+app.use((req, res, next) => {
+    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+    next();
+});
+
+// Configure bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Configure CORs middleware
+app.use(cors());
+
+// Configure routes
+const api = require('./routes/routes');
+app.use('/api/v1', api);
+
+// Middleware to serve compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/builld', 'index.html'));
+    });
+};
+
+// Config server to listen on port
+app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
